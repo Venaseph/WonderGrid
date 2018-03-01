0d0f5addff9cc6170eba0ecab33e555d21d6d164
Chris Peragine <chrisperagine@Chriss-Mac.local>
2018-03-01 09:03:13 -0800

    more box calls

--- WonderGrid/ViewController.swift
+++ WonderGrid/ViewController.swift
@@ -18,20 +18,19 @@ class ViewController: UIViewController {
         let x = view.frame.width
         let y = view.frame.height
         
-        let boxSize = getBoxSize(x: x, y: y)
+        let boxSizeCount = getBoxSizeCount(x: x, y: y)
         
-        //num of boxes per col/row
-        let countViewCol = 15
-        let countViewRow = 20
+        //num of boxes per col/row for loop
+        let countViewCol = Int(boxSizeCount.1)
+        let countViewRow = Int(boxSizeCount.2)
+        
         //handle for all size devices with view.frame to get box correct box size
-        let width = x / CGFloat(countViewCol)
-        let height = y / CGFloat(countViewRow)
+        let width = boxSizeCount.0
+        let height = boxSizeCount.0
         
         //renderCells
-        for j in 0...countViewRow {
-            //for vert
-            for i in 0...countViewCol {
-                //for horiz
+        for j in 0...countViewRow { //for vert
+            for i in 0...countViewCol { //for horiz
                 let cellView = UIView()
                 //generate random color value for each box
                 cellView.backgroundColor = randomColor()
@@ -49,28 +48,47 @@ class ViewController: UIViewController {
         
     }
     
-    func getBoxSize (x: CGFloat, y: CGFloat) -> CGFloat {
+    fileprivate func getBoxSizeCount (x: CGFloat, y: CGFloat) -> (CGFloat, CGFloat, CGFloat) {
         
-        // compute number of row and cols, and box size
+        // compute number of row and cols, and box size for correct device view.frame
         let ratio = x / y
+        let n = CGFloat(30)
         let ncols = sqrt(30 * ratio)
         let nrows = 30 / ncols
         
         //find correct height fill
         var nrows1 = ceil(nrows)
-        var ncols1 = ceil(30 / nrows1)
+        var ncols1 = ceil(n / nrows1)
         while (nrows1 * ratio < ncols) {
             nrows1 += 1
-            ncols1 = ceil(30 / nrows1)
+            ncols1 = ceil(n / nrows1)
         }
-        let cell_size1 = y / nrows1
+        let cellSize1 = y / nrows1
         
         //find correct width fill
-        let ncols2 = ceil(ncols)
+        var ncols2 = ceil(ncols)
+        var nrows2 = ceil(n / ncols2)
+        while (ncols2 < nrows2 * ratio) {
+            ncols2 += 1
+            nrows2 = ceil(n / ncols2)
+        }
+        let cellSize2 = x / ncols2
         
-        return 0
+        //Find optimal size/counts conditional
+        var nrow: CGFloat
+        var ncol: CGFloat
+        var cellSize: CGFloat
+        if (cellSize1 < cellSize2) {
+            nrow = nrows2
+            ncol = ncols2
+            cellSize = cellSize2
+        } else {
+            nrow = nrows1
+            ncol = ncols1
+            cellSize = cellSize1
+        }
         
-        
+        return (cellSize, ncol, nrow)
     }
     
     @objc func handlePan(gesture: UIPanGestureRecognizer) {
